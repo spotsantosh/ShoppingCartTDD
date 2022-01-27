6b3e8ad5d1fa689d68b027301fcc60ab5ac2720f
santosh shitole <spotsantosh@gmail.com>
2022-01-27 00:04:04 -0500

    initial commit

---
+++ test/java/com/santosh/shoppingcart/ShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
